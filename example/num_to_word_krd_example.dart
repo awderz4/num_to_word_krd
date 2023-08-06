0e9fee1bc0eae551a8fa585fce87fa008926a020
awderz4 <awderkawa9494@gmail.com>
2023-08-06 17:55:50 +0300

    #

--- example/num_to_word_krd_example.dart
+++ example/num_to_word_krd_example.dart
@@ -1,7 +1,5 @@
 import 'package:num_to_word_krd/num_to_word_krd.dart';
 
 void main() {
-  for (var i = 100000; i <= 1000000; i = i + 10500) {
-    print('$i = ${NumberToWordKrdConverter.convert(i)}');
-  }
+  print(NumberToWordKrdConverter.convert(999129));
 }
